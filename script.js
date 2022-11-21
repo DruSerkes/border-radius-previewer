ad21f78d63eed002e84161b2e0f9370433b4ab3a
Dru Serkes <druserkes@gmail.com>
2022-11-20 23:42:31 -0500

    add button to copy css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const topLeft = document.querySelector('input[name=top-left]');
 const topRight = document.querySelector('input[name=top-right]');
 const bottomRight = document.querySelector('input[name=bottom-right]');
 const bottomLeft = document.querySelector('input[name=bottom-left]');
+const copyCssButton = document.querySelector('#copy-css-button');
 
 const handleChangeValue = (e) => {
   if (e.target.value[0] === '-') e.target.value = e.target.value.slice(1);
@@ -10,7 +11,18 @@ const handleChangeValue = (e) => {
   let { name, value } = e.target;
   if (!value) value = 0;
 
+  const valueElement = document.querySelector(`#${name}-value`);
+  valueElement.textContent = value;
+
   rectangle.style[`border-${name}-radius`] = `${value}px`;
+};
+
+const handleCopyCss = () => {
+  const paragraphs = Array.from(document.querySelectorAll('.resulting-css'));
+  const resultingCss = paragraphs.reduce((result, next) => {
+    return result += `${next.textContent};\n`;
+  }, '');
+  navigator.clipboard.writeText(resultingCss);
 };
 
 topLeft.addEventListener('change', handleChangeValue);
@@ -24,3 +36,5 @@ bottomRight.addEventListener('keyup', handleChangeValue);
 
 bottomLeft.addEventListener('change', handleChangeValue);
 bottomLeft.addEventListener('keyup', handleChangeValue);
+
+copyCssButton.addEventListener('click', handleCopyCss);
